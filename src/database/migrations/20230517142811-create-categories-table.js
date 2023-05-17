9962b306cc21a59451e04c9c2fec30e5888f16a1
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-05-17 12:20:07 -0300

    Criando o primeiro resource

--- src/database/migrations/20230517142811-create-categories-table.js
+++ src/database/migrations/20230517142811-create-categories-table.js
@@ -1,21 +1,34 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(`categories`, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      position: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(`categories`);
+  },
 };
